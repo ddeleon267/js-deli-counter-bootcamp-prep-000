4646b9e05b6619eb964ac2673fa5620c4b252c94
Wil Cook <wil.cook.is@gmail.com>
2018-11-13 15:48:49 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,29 @@
+function takeANumber (queue, newPerson) {
+  queue.push(newPerson);
+  return `Welcome, ${newPerson}. You are number ${queue.length} in line.`
+}
+
+function nowServing (katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    return `Currently serving ${katzDeliLine[0]}.`
+    katzDeliLine.shift()
+  } else {
+    return "There is nobody waiting to be served!"
+  }
+}
+
+
+var array = ['Wiljago', 'Orin', 'Jory', 'Nikolous', 'Hanika', 'Anjie', 'Evan']
+
+function currentLine (queue) {
+  if (queue.length > 0) {
+    console.log(`The line is currently: 1. ${queue.join(`,  ${queue.indexOf(`/*/`)}. `)}.`)
+
+  }
+}
+
+currentLine(array);
+
+
+
+// queue.slice(0, queue.length - 1).join(', ') + ", and " + queue.slice(-1));
